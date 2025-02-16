8bc16262881e8b5a1bafe802ee36492afb20cdc0
Akira Kawata <akirakawata1@gmail.com>
2025-02-16 15:41:54 +0900

    Fix script

--- run_format_and_lint.sh
+++ run_format_and_lint.sh
@@ -4,7 +4,7 @@ set -eux -o pipefail
 
 VERIBLE_DIR=/tmp/verible-v0.0-3933-g0e3fe53a-linux-static-x86_64
 
-if [ -d ${VERIBLE_DIR} ]; then
+if [ ! -d ${VERIBLE_DIR} ]; then
     mkdir -p ${VERIBLE_DIR}
     wget https://github.com/chipsalliance/verible/releases/download/v0.0-3933-g0e3fe53a/verible-v0.0-3933-g0e3fe53a-linux-static-x86_64.tar.gz -O ${VERIBLE_DIR}/verible.tar.gz
     tar -xvf ${VERIBLE_DIR}/verible.tar.gz -C ${VERIBLE_DIR}
